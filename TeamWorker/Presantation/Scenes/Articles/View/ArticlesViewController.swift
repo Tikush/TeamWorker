1d93048a73844aaa54f99a4ca764a6366b313387
Tikush <tikush23@gmail.com>
2021-07-05 19:19:07 +0400

    - add articlesView Controller

---
+++ TeamWorker/Presantation/Scenes/Articles/View/ArticlesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ArticlesViewController.swift
+//  TeamWorker
+//
+//  Created by Tiko on 05.07.21.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Life Cyrcles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
