a534bdaa03def2314a2449fc014914a7cce6e6d3
MacBook <>
2021-07-05 20:17:32 +0400

    commit detail page

--- TeamWorker/Detail/DetailViewController.swift
+++ TeamWorker/Detail/DetailViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var labelContent: UILabel!
+    @IBOutlet weak var labelDate: UILabel!
+    @IBOutlet weak var labelAuthorName: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    @IBAction func onBack(_ sender: Any) {
     }
-    */
-
+    @IBAction func onBookmarks(_ sender: Any) {
+    }
+    
 }
+
+
